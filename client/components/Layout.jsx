6ff3d1a6c820ef3f10b980db851e393eef51896a
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-09-18 13:05:17 -0400

    updated vite.config.js

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Layout() {
     return (
         <>
+        <header>
+        <img src={bianca_logo} alt="Logo" className="logo" />
+        </header>
             <h1>My Portfolio</h1>
             <nav>
                 <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link> | <Link to="/project">Project</Link> | <Link to="/contact">Contact</Link>
