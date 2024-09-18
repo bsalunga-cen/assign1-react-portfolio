c7eb6c2d66a4d7199d0a8cc0dfc6cfa4a7f3f196
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-09-18 13:12:31 -0400

    updated home and layout

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,22 +1,22 @@
-// import React from 'react';
+import React from 'react';
 
-// const Home = () => (
-//   <div>
-//     <h1>Welcome to My Portfolio</h1>
-//     <h3>Mission Statement</h3>
-//     <p>As a passionate and dedicated Software Engineering Technician, my mission is to leverage technology to create innovative solutions that enhance user experiences and solve real-world problems. I strive to continuously expand my knowledge and skills in software development, focusing on building efficient, scalable, and user-friendly applications. </p>
-//     <p>I am committed to collaborating with diverse teams to foster creativity and drive impactful projects. My goal is to contribute to the tech community by developing software that not only meets functional requirements but also prioritizes accessibility and sustainability. Through my work, I aim to empower users and improve their daily lives, while embracing the challenges and opportunities that the ever-evolving tech landscape presents. Feel free to adjust any part of the statement to better reflect your personal values and aspirations.</p>
-//     <a href="/about">Learn More About Me</a>
-//   </div>
-// );
-// export default Home;
+const Home = () => (
+  <div>
+    <h1>Welcome to My Portfolio</h1>
+    <h3>Mission Statement</h3>
+    <p>As a passionate and dedicated Software Engineering Technician, my mission is to leverage technology to create innovative solutions that enhance user experiences and solve real-world problems. I strive to continuously expand my knowledge and skills in software development, focusing on building efficient, scalable, and user-friendly applications. </p>
+    <p>I am committed to collaborating with diverse teams to foster creativity and drive impactful projects. My goal is to contribute to the tech community by developing software that not only meets functional requirements but also prioritizes accessibility and sustainability. Through my work, I aim to empower users and improve their daily lives, while embracing the challenges and opportunities that the ever-evolving tech landscape presents. Feel free to adjust any part of the statement to better reflect your personal values and aspirations.</p>
+    <a href="/about">Learn More About Me</a>
+  </div>
+);
+export default Home;
 
 
 
-export default function Home() {
-    return (
-    <p>Hello World! ffd</p>, 
-        <h2>Mission Statement</h2>
-    )
-}
+// export default function Home() {
+//     return (
+//     <p>Hello World! ffd</p>, 
+//         <h2>Mission Statement</h2>
+//     )
+// }
 
