11d6bd512e8262440155254506e5a9dabcd47b31
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-09-18 22:26:45 -0400

    update all pages

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../src/assets/logo.png';
 
 export default function Layout() {
     return (
         <>
-            <h1>My Portfolio</h1>
-            <nav>
-                <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link> | <Link to="/project">Project</Link> | <Link to="/contact">Contact</Link>
-            </nav>
-            <br />
+            <div className="navbar">
+                <img className='my-logo' src={logo} alt="Logo" />
+                <nav>
+                    <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/services">Services</Link> | <Link to="/project">Project</Link> | <Link to="/contact">Contact </Link>
+                </nav>
+            </div>
             <hr />
 
         </>
