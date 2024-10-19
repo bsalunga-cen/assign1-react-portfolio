c7a2fb4d106a7513c0f5075f889af0df13a7a8b1
bsalunga-cen <bsalunga@my.centennialcollege.ca>
2024-10-19 13:56:02 -0400

    setup completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "assign1-react-portfolio",
+    base: "/assign2-portfolio-backend",
   plugins: [react()],
 })
